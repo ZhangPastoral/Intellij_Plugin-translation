06e059d3ebb2e82279f5b1db848b4cca7e6c1141
GitHub Action <action@github.com>
2022-06-27 06:17:31 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/zhangpastoral/intellijplugintranslation/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zhangpastoral.intellijplugintranslation.services
+
+import com.github.zhangpastoral.intellijplugintranslation.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
